e0295c367e601bf783c67d4bc51342f654b77edc
leonardloja <82754688+leonardloja@users.noreply.github.com>
2023-02-26 22:22:26 +0800

    add bunifu pages

--- Main.Designer.vb
+++ Main.Designer.vb
@@ -23,6 +23,8 @@ Partial Class Main
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim Animation1 As Utilities.BunifuPages.BunifuAnimatorNS.Animation = New Utilities.BunifuPages.BunifuAnimatorNS.Animation()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
         Me.Guna2Elipse1 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
         Me.panelTopLogo = New Guna.UI2.WinForms.Guna2Panel()
         Me.Guna2ControlBox1 = New Guna.UI2.WinForms.Guna2ControlBox()
@@ -44,11 +46,21 @@ Partial Class Main
         Me.Guna2Button7 = New Guna.UI2.WinForms.Guna2Button()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.panelMain = New System.Windows.Forms.Panel()
+        Me.bunifuPagesMain = New Bunifu.UI.WinForms.BunifuPages()
+        Me.dashboard = New System.Windows.Forms.TabPage()
+        Me.files = New System.Windows.Forms.TabPage()
+        Me.history = New System.Windows.Forms.TabPage()
+        Me.users = New System.Windows.Forms.TabPage()
+        Me.settings = New System.Windows.Forms.TabPage()
+        Me.currentUser = New System.Windows.Forms.TabPage()
         Me.panelTopLogo.SuspendLayout()
         Me.panelLeft.SuspendLayout()
         Me.Guna2Panel1.SuspendLayout()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.panelMain.SuspendLayout()
+        Me.bunifuPagesMain.SuspendLayout()
         Me.SuspendLayout()
         '
         'Guna2Elipse1
@@ -73,7 +85,7 @@ Partial Class Main
         Me.Guna2ControlBox1.BackColor = System.Drawing.Color.White
         Me.Guna2ControlBox1.FillColor = System.Drawing.Color.Transparent
         Me.Guna2ControlBox1.IconColor = System.Drawing.Color.Black
-        Me.Guna2ControlBox1.Location = New System.Drawing.Point(1645, 4)
+        Me.Guna2ControlBox1.Location = New System.Drawing.Point(1647, 4)
         Me.Guna2ControlBox1.Name = "Guna2ControlBox1"
         Me.Guna2ControlBox1.Size = New System.Drawing.Size(45, 29)
         Me.Guna2ControlBox1.TabIndex = 0
@@ -107,6 +119,7 @@ Partial Class Main
         Me.Guna2Button4.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton
         Me.Guna2Button4.CheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(191, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(254, Byte), Integer))
         Me.Guna2Button4.CheckedState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(99, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Guna2Button4.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Guna2Button4.CustomImages.CheckedImage = Global.FilioSystem.My.Resources.Resources.settings_checked
         Me.Guna2Button4.CustomImages.HoveredImage = Global.FilioSystem.My.Resources.Resources.settings_checked
         Me.Guna2Button4.CustomImages.Image = Global.FilioSystem.My.Resources.Resources.settings_default
@@ -148,6 +161,7 @@ Partial Class Main
         Me.Guna2Button3.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton
         Me.Guna2Button3.CheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(191, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(254, Byte), Integer))
         Me.Guna2Button3.CheckedState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(99, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Guna2Button3.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Guna2Button3.CustomImages.CheckedImage = Global.FilioSystem.My.Resources.Resources.home_checked
         Me.Guna2Button3.CustomImages.HoveredImage = Global.FilioSystem.My.Resources.Resources.home_checked
         Me.Guna2Button3.CustomImages.Image = Global.FilioSystem.My.Resources.Resources.home_default
@@ -178,6 +192,7 @@ Partial Class Main
         Me.Guna2Button2.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton
         Me.Guna2Button2.CheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(191, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(254, Byte), Integer))
         Me.Guna2Button2.CheckedState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(99, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Guna2Button2.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Guna2Button2.CustomImages.CheckedImage = Global.FilioSystem.My.Resources.Resources.folder_checked
         Me.Guna2Button2.CustomImages.HoveredImage = Global.FilioSystem.My.Resources.Resources.folder_checked1
         Me.Guna2Button2.CustomImages.Image = Global.FilioSystem.My.Resources.Resources.folder_default
@@ -208,6 +223,7 @@ Partial Class Main
         Me.Guna2Button1.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton
         Me.Guna2Button1.CheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(191, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(254, Byte), Integer))
         Me.Guna2Button1.CheckedState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(99, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Guna2Button1.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Guna2Button1.CustomImages.CheckedImage = Global.FilioSystem.My.Resources.Resources.dashboard_checked
         Me.Guna2Button1.CustomImages.HoveredImage = Global.FilioSystem.My.Resources.Resources.dashboard_checked
         Me.Guna2Button1.CustomImages.Image = Global.FilioSystem.My.Resources.Resources.dashboard_default
@@ -244,7 +260,7 @@ Partial Class Main
         Me.Guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox
         Me.Guna2ControlBox2.FillColor = System.Drawing.Color.Transparent
         Me.Guna2ControlBox2.IconColor = System.Drawing.Color.Black
-        Me.Guna2ControlBox2.Location = New System.Drawing.Point(1594, 5)
+        Me.Guna2ControlBox2.Location = New System.Drawing.Point(1600, 5)
         Me.Guna2ControlBox2.Name = "Guna2ControlBox2"
         Me.Guna2ControlBox2.Size = New System.Drawing.Size(45, 29)
         Me.Guna2ControlBox2.TabIndex = 2
@@ -252,6 +268,7 @@ Partial Class Main
         'panelTop
         '
         Me.panelTop.BackColor = System.Drawing.Color.White
+        Me.panelTop.Cursor = System.Windows.Forms.Cursors.NoMove2D
         Me.panelTop.CustomBorderColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(232, Byte), Integer), CType(CType(240, Byte), Integer))
         Me.panelTop.CustomBorderThickness = New System.Windows.Forms.Padding(0, 0, 0, 1)
         Me.panelTop.Dock = System.Windows.Forms.DockStyle.Top
@@ -299,6 +316,7 @@ Partial Class Main
         Me.Guna2Button5.BorderRadius = 8
         Me.Guna2Button5.CheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(191, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(254, Byte), Integer))
         Me.Guna2Button5.CheckedState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(99, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Guna2Button5.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Guna2Button5.CustomImages.CheckedImage = Global.FilioSystem.My.Resources.Resources.logout_checked
         Me.Guna2Button5.CustomImages.HoveredImage = Global.FilioSystem.My.Resources.Resources.logout_checked
         Me.Guna2Button5.CustomImages.Image = Global.FilioSystem.My.Resources.Resources.logout_default
@@ -329,6 +347,7 @@ Partial Class Main
         Me.Guna2Button6.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton
         Me.Guna2Button6.CheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(191, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(254, Byte), Integer))
         Me.Guna2Button6.CheckedState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(99, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Guna2Button6.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Guna2Button6.CustomImages.CheckedImage = Global.FilioSystem.My.Resources.Resources.user_checked
         Me.Guna2Button6.CustomImages.HoveredImage = Global.FilioSystem.My.Resources.Resources.user_checked
         Me.Guna2Button6.CustomImages.Image = Global.FilioSystem.My.Resources.Resources.user_default
@@ -368,6 +387,7 @@ Partial Class Main
         Me.Guna2Button7.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton
         Me.Guna2Button7.CheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(191, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(254, Byte), Integer))
         Me.Guna2Button7.CheckedState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(99, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Guna2Button7.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Guna2Button7.CustomImages.CheckedImage = Global.FilioSystem.My.Resources.Resources.history_checked
         Me.Guna2Button7.CustomImages.HoveredImage = Global.FilioSystem.My.Resources.Resources.history_checked
         Me.Guna2Button7.CustomImages.Image = Global.FilioSystem.My.Resources.Resources.history_default
@@ -411,12 +431,120 @@ Partial Class Main
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
+        'panelMain
+        '
+        Me.panelMain.Controls.Add(Me.bunifuPagesMain)
+        Me.panelMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.panelMain.Location = New System.Drawing.Point(300, 37)
+        Me.panelMain.Margin = New System.Windows.Forms.Padding(0)
+        Me.panelMain.Name = "panelMain"
+        Me.panelMain.Size = New System.Drawing.Size(1400, 885)
+        Me.panelMain.TabIndex = 5
+        '
+        'bunifuPagesMain
+        '
+        Me.bunifuPagesMain.Alignment = System.Windows.Forms.TabAlignment.Bottom
+        Me.bunifuPagesMain.AllowTransitions = True
+        Me.bunifuPagesMain.Controls.Add(Me.dashboard)
+        Me.bunifuPagesMain.Controls.Add(Me.files)
+        Me.bunifuPagesMain.Controls.Add(Me.users)
+        Me.bunifuPagesMain.Controls.Add(Me.history)
+        Me.bunifuPagesMain.Controls.Add(Me.settings)
+        Me.bunifuPagesMain.Controls.Add(Me.currentUser)
+        Me.bunifuPagesMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.bunifuPagesMain.Location = New System.Drawing.Point(0, 0)
+        Me.bunifuPagesMain.Multiline = True
+        Me.bunifuPagesMain.Name = "bunifuPagesMain"
+        Me.bunifuPagesMain.Page = Me.dashboard
+        Me.bunifuPagesMain.PageIndex = 0
+        Me.bunifuPagesMain.PageName = "dashboard"
+        Me.bunifuPagesMain.PageTitle = "dashboard"
+        Me.bunifuPagesMain.SelectedIndex = 0
+        Me.bunifuPagesMain.Size = New System.Drawing.Size(1400, 885)
+        Me.bunifuPagesMain.TabIndex = 0
+        Animation1.AnimateOnlyDifferences = False
+        Animation1.BlindCoeff = CType(resources.GetObject("Animation1.BlindCoeff"), System.Drawing.PointF)
+        Animation1.LeafCoeff = 0!
+        Animation1.MaxTime = 1.0!
+        Animation1.MinTime = 0!
+        Animation1.MosaicCoeff = CType(resources.GetObject("Animation1.MosaicCoeff"), System.Drawing.PointF)
+        Animation1.MosaicShift = CType(resources.GetObject("Animation1.MosaicShift"), System.Drawing.PointF)
+        Animation1.MosaicSize = 0
+        Animation1.Padding = New System.Windows.Forms.Padding(0, 0, 0, 0)
+        Animation1.RotateCoeff = 0!
+        Animation1.RotateLimit = 0!
+        Animation1.ScaleCoeff = CType(resources.GetObject("Animation1.ScaleCoeff"), System.Drawing.PointF)
+        Animation1.SlideCoeff = CType(resources.GetObject("Animation1.SlideCoeff"), System.Drawing.PointF)
+        Animation1.TimeCoeff = 0!
+        Animation1.TransparencyCoeff = 0!
+        Me.bunifuPagesMain.Transition = Animation1
+        Me.bunifuPagesMain.TransitionType = Utilities.BunifuPages.BunifuAnimatorNS.AnimationType.Custom
+        '
+        'dashboard
+        '
+        Me.dashboard.BackColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(249, Byte), Integer))
+        Me.dashboard.Location = New System.Drawing.Point(4, 4)
+        Me.dashboard.Name = "dashboard"
+        Me.dashboard.Padding = New System.Windows.Forms.Padding(3)
+        Me.dashboard.Size = New System.Drawing.Size(1392, 858)
+        Me.dashboard.TabIndex = 0
+        Me.dashboard.Text = "dashboard"
+        '
+        'files
+        '
+        Me.files.BackColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(249, Byte), Integer))
+        Me.files.Location = New System.Drawing.Point(4, 4)
+        Me.files.Name = "files"
+        Me.files.Padding = New System.Windows.Forms.Padding(3)
+        Me.files.Size = New System.Drawing.Size(1392, 858)
+        Me.files.TabIndex = 1
+        Me.files.Text = "files"
+        '
+        'history
+        '
+        Me.history.Location = New System.Drawing.Point(4, 4)
+        Me.history.Name = "history"
+        Me.history.Padding = New System.Windows.Forms.Padding(3)
+        Me.history.Size = New System.Drawing.Size(1392, 858)
+        Me.history.TabIndex = 2
+        Me.history.Text = "history"
+        Me.history.UseVisualStyleBackColor = True
+        '
+        'users
+        '
+        Me.users.Location = New System.Drawing.Point(4, 4)
+        Me.users.Name = "users"
+        Me.users.Padding = New System.Windows.Forms.Padding(3)
+        Me.users.Size = New System.Drawing.Size(1392, 858)
+        Me.users.TabIndex = 3
+        Me.users.Text = "users"
+        Me.users.UseVisualStyleBackColor = True
+        '
+        'settings
+        '
+        Me.settings.Location = New System.Drawing.Point(4, 4)
+        Me.settings.Name = "settings"
+        Me.settings.Size = New System.Drawing.Size(1392, 858)
+        Me.settings.TabIndex = 4
+        Me.settings.Text = "settings"
+        Me.settings.UseVisualStyleBackColor = True
+        '
+        'currentUser
+        '
+        Me.currentUser.Location = New System.Drawing.Point(4, 4)
+        Me.currentUser.Name = "currentUser"
+        Me.currentUser.Size = New System.Drawing.Size(1392, 858)
+        Me.currentUser.TabIndex = 5
+        Me.currentUser.Text = "currentUser"
+        Me.currentUser.UseVisualStyleBackColor = True
+        '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(249, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(1700, 956)
+        Me.Controls.Add(Me.panelMain)
         Me.Controls.Add(Me.Guna2Panel1)
         Me.Controls.Add(Me.Guna2ControlBox2)
         Me.Controls.Add(Me.Guna2ControlBox1)
@@ -435,6 +563,8 @@ Partial Class Main
         Me.Guna2Panel1.PerformLayout()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.panelMain.ResumeLayout(False)
+        Me.bunifuPagesMain.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -460,4 +590,12 @@ Partial Class Main
     Friend WithEvents Guna2Button5 As Guna.UI2.WinForms.Guna2Button
     Friend WithEvents Guna2Separator2 As Guna.UI2.WinForms.Guna2Separator
     Friend WithEvents Guna2Button7 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents panelMain As Panel
+    Friend WithEvents bunifuPagesMain As Bunifu.UI.WinForms.BunifuPages
+    Friend WithEvents dashboard As TabPage
+    Friend WithEvents files As TabPage
+    Friend WithEvents history As TabPage
+    Friend WithEvents users As TabPage
+    Friend WithEvents settings As TabPage
+    Friend WithEvents currentUser As TabPage
 End Class
